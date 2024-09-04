e1d422eb7eb70b782215278077e8bebe48013aa0
Kresna Satya <kresnasatya72@gmail.com>
2024-09-04 11:24:47 +0800

    install octokit and implement endpoint API.
    - tags => api/tags
    - diff => api/diff
      diff accept two parameters: source and target

---
+++ src/github.js
@@ -0,0 +1,87 @@
+import { Octokit } from "octokit";
+
+export async function allTags(repository) {
+	const octokit = new Octokit();
+
+	const [owner, repo] = repository.split('/', 2);
+
+	/**
+	 * @type {any[]}
+	 */
+	let tagNames = [];
+	for await (const response of octokit.paginate.iterator(
+		octokit.rest.repos.listTags,
+		{ owner, per_page: 100, repo }
+	)) {
+		tagNames = tagNames.concat(response.data.map((data) => data.name));
+	}
+
+	return tagNames;
+}
+
+export async function getDiff(repository, sourceVersion, targetVersion) {
+	try {
+		const octokit = new Octokit();
+
+		const [owner, repo] = repository.split('/', 2);
+		/**
+		 * @type {any[]}
+		 */
+		let files = [];
+		// Exclude filename when get patch between two versions.
+		const excludeFilenames = ['CHANGELOG.md', '.github', 'README.md'];
+
+		for await (const response of octokit.paginate.iterator(
+			octokit.rest.repos.compareCommitsWithBasehead,
+			{
+				basehead: `${sourceVersion}...${targetVersion}`,
+				owner,
+				per_page: 100,
+				repo
+			}
+		)) {
+			if (response.data.files) {
+				files = files.concat(response.data.files).filter((file) => {
+					return !excludeFilenames.some(prefix => file.filename.startsWith(prefix));
+				});
+			}
+		}
+
+		const result = files.map((/** @type {{ sha: any; filename: any; patch: string; }} */ item) => {
+			return {
+				sha: item.sha,
+				filename: item.filename,
+				source_url: `https://github.com/${repository}/blob/${sourceVersion}/${item.filename}`,
+				target_url: `https://github.com/${repository}/blob/${targetVersion}/${item.filename}`,
+				lines: parsedLines(item.patch),
+			}
+		});
+
+		return result;
+	} catch (error) {
+		throw new Error(`Cannot get diff between source version: ${sourceVersion} and target version: ${targetVersion}`);
+	}
+}
+
+function parsedLines(diffString) {
+	const lines = diffString.split('\n');
+
+    const parsedLines = lines.map((/** @type {string} */ line, /** @type {number} */ index) => {
+        let status = 'unchanged';
+
+        // Check the first character of the line to determine the status
+        if (line.startsWith('+')) {
+            status = 'add';
+        } else if (line.startsWith('-')) {
+            status = 'remove';
+        }
+
+        return {
+            number: index + 1,
+            text: line,
+            status: status
+        }
+    });
+
+    return parsedLines;
+}
--- src/index.js
+++ src/index.js
@@ -8,8 +8,80 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { allTags, getDiff } from "./github.js";
+
 export default {
 	async fetch(request, env, ctx) {
+		const url = new URL(request.url);
+
+		if (url.pathname === '/api/tags') {
+			const cacheKey = new Request(url.toString(), request);
+			const cache = caches.default;
+			let response = await cache.match(cacheKey);
+
+			if (!response) {
+				let json = await allTags('laravel/laravel');
+
+				response = new Response(JSON.stringify(json));
+
+				response.headers.set('Cache-Control', 'public, max-age=3600');
+				response.headers.set('Content-Type', 'application/json');
+
+				// Put the response in the cache
+				ctx.waitUntil(cache.put(cacheKey, response.clone()));
+			}
+
+			return response;
+		}
+
+		// api/patch?source=v11.0.0&target=v12.0.0
+		if (url.pathname === '/api/diff') {
+			const sourceVersion = url.searchParams.get('source');
+			const targetVersion = url.searchParams.get('target');
+
+			if (!sourceVersion && !targetVersion) {
+				return new Response('Missing source or target query parameter', {
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					status: 400
+				});
+			}
+
+			const cacheKey = new Request(url.toString(), request);
+			const cache = caches.default;
+			let response = await cache.match(cacheKey);
+
+			if (!response) {
+				try {
+					let json = await getDiff('laravel/laravel', sourceVersion, targetVersion);
+
+					response = new Response(JSON.stringify(json), {
+						headers: {
+							'Cache-Control': 'public, max-age=3600',
+							'Content-Type': 'application/json'
+						}
+					});
+
+					// Put the response in the cache
+					ctx.waitUntil(cache.put(cacheKey, response.clone()));
+				} catch (error) {
+					response = new Response(JSON.stringify({ "message": error.message }), {
+						headers: {
+							'Cache-Control': 'public, max-age=3600',
+							'Content-Type': 'application/json'
+						},
+						status: 404
+					});
+
+					// Put the response in the cache
+					ctx.waitUntil(cache.put(cacheKey, response.clone()));
+				}
+			}
+
+			return response;
+		}
+
 		return new Response('Hello World!');
 	},
 };
